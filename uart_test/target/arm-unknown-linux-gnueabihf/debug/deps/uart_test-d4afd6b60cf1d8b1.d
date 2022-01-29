2dd9c0940de2554d40689b23b151e82cda08b7e0
danclan13 <danilasniff@gmail.com>
2022-01-29 19:36:19 +0000

    Deleting files

--- uart_test/target/arm-unknown-linux-gnueabihf/debug/deps/uart_test-d4afd6b60cf1d8b1.d
+++
@@ -1,5 +0,0 @@
-/home/dpop/Documents/GitHub/Mike-core/uart_test/target/arm-unknown-linux-gnueabihf/debug/deps/uart_test-d4afd6b60cf1d8b1: src/main.rs
-
-/home/dpop/Documents/GitHub/Mike-core/uart_test/target/arm-unknown-linux-gnueabihf/debug/deps/uart_test-d4afd6b60cf1d8b1.d: src/main.rs
-
-src/main.rs:
