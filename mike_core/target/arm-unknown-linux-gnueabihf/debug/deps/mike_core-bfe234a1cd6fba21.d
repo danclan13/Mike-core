a19c8bcfa07ee694a2b0b3b56274aba65174d474
danclan13 <danilasniff@gmail.com>
2022-01-30 10:45:08 +0000

    Updating the code

---
+++ mike_core/target/arm-unknown-linux-gnueabihf/debug/deps/mike_core-bfe234a1cd6fba21.d
@@ -0,0 +1,5 @@
+/home/dpop/Documents/GitHub/Mike-core/mike_core/target/arm-unknown-linux-gnueabihf/debug/deps/mike_core-bfe234a1cd6fba21: src/main.rs
+
+/home/dpop/Documents/GitHub/Mike-core/mike_core/target/arm-unknown-linux-gnueabihf/debug/deps/mike_core-bfe234a1cd6fba21.d: src/main.rs
+
+src/main.rs:
